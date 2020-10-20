2f1f8e5e96d46210a52c9c7c68da20c8082035b2
Mikel <mikelgmh@gmail.com>
2020-10-20 08:44:15 +0200

    first commit

---
+++ src/signupsignin/server/SignUpSignInServer.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package signupsignin.server;
+
+/**
+ *
+ * @author Mikel
+ */
+public class SignUpSignInServer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
