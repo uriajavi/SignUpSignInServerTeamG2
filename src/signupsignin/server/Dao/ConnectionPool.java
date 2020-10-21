e8799fb6e8f3a9667384defee8057c63d90bf220
Imanol <2dam@HZ341338.TartangaLH.eus>
2020-10-21 14:21:25 +0200

    Connection Pool con ArrayList. Buscar como hacer de otra manera.

--- src/signupsignin/server/Dao/ConnectionPool.java
+++ src/signupsignin/server/Dao/ConnectionPool.java
@@ -5,10 +5,87 @@
  */
 package signupsignin.server.Dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
- * @author Mikel
+ * @author Imanol
  */
 public class ConnectionPool {
     
+    private List<Connection>availableConnections = new ArrayList<Connection>();
+    private List<Connection>usedConnections = new ArrayList<Connection>();
+    private final int MAX_CONNECTIONS = 5;
+
+	private String URL;
+	private String USERID;
+	private String PASSWORD;
+
+
+	/** Initialize all 5 Connections and put them in the Pool **/
+	public ConnectionPool(String Url, String UserId, String password) throws SQLException {
+		this.URL = Url;
+		this.USERID = UserId;
+		this.PASSWORD = password;
+
+		for (int count = 0; count <MAX_CONNECTIONS; count++) {
+			availableConnections.add(this.createConnection());
+		}
+
+	}
+
+
+
+
+
+
+/** Private function, 
+	used by the Pool to create new connection internally **/
+
+	private Connection createConnection() throws SQLException {
+		return DriverManager.getConnection(this.URL, this.USERID, this.PASSWORD);
+	}
+
+
+
+
+	/** Public function, used by us to get connection from Pool **/
+	public Connection getConnection() {
+		if (availableConnections.size() == 0) {
+			System.out.println("All connections are Used !!");
+			return null;
+		} else {
+			Connection con = 
+			availableConnections.remove(
+				availableConnections.size() - 1);
+			usedConnections.add(con);
+			return con;
+		}
+	}
+
+
+
+	/** Public function, to return connection back to the Pool **/
+	public boolean releaseConnection(Connection con) {
+		if (null != con) {
+			usedConnections.remove(con);
+			availableConnections.add(con);
+			return true;
+		}
+		return false;
+	}
+
+
+
+
+
+	/** Utility function to check the number of Available Connections **/
+	public int getFreeConnectionCount() {
+		return availableConnections.size();
+	}
+
 }
