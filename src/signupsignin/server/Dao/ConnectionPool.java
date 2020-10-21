9164455e5c808e0abeb5fd34d65816652a0bf421
moxarlos <56909426+moxarlos@users.noreply.github.com>
2020-10-21 21:31:06 +0200

    Pool actualizado.
    Pool con BasicDataSource. Faltan incluir valores adecuados y establecer restricciones y propiedades del pool.

--- src/signupsignin/server/Dao/ConnectionPool.java
+++ src/signupsignin/server/Dao/ConnectionPool.java
@@ -10,82 +10,34 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import javax.sql.DataSource;
+import org.apache.commons.dbcp2.BasicDataSource;
 
 /**
  *
  * @author Imanol
  */
 public class ConnectionPool {
+    private static BasicDataSource ds = null;
     
-    private List<Connection>availableConnections = new ArrayList<Connection>();
-    private List<Connection>usedConnections = new ArrayList<Connection>();
-    private final int MAX_CONNECTIONS = 5;
-
-	private String URL;
-	private String USERID;
-	private String PASSWORD;
-
-
-	/** Initialize all 5 Connections and put them in the Pool **/
-	public ConnectionPool(String Url, String UserId, String password) throws SQLException {
-		this.URL = Url;
-		this.USERID = UserId;
-		this.PASSWORD = password;
-
-		for (int count = 0; count <MAX_CONNECTIONS; count++) {
-			availableConnections.add(this.createConnection());
-		}
-
-	}
-
-
-
-
-
-
-/** Private function, 
-	used by the Pool to create new connection internally **/
-
-	private Connection createConnection() throws SQLException {
-		return DriverManager.getConnection(this.URL, this.USERID, this.PASSWORD);
-	}
-
-
-
-
-	/** Public function, used by us to get connection from Pool **/
-	public Connection getConnection() {
-		if (availableConnections.size() == 0) {
-			System.out.println("All connections are Used !!");
-			return null;
-		} else {
-			Connection con = 
-			availableConnections.remove(
-				availableConnections.size() - 1);
-			usedConnections.add(con);
-			return con;
-		}
-	}
-
-
-
-	/** Public function, to return connection back to the Pool **/
-	public boolean releaseConnection(Connection con) {
-		if (null != con) {
-			usedConnections.remove(con);
-			availableConnections.add(con);
-			return true;
-		}
-		return false;
-	}
-
-
-
-
-
-	/** Utility function to check the number of Available Connections **/
-	public int getFreeConnectionCount() {
-		return availableConnections.size();
-	}
-
+    public static DataSource getDataSource(){
+    
+        //Establecer valores
+    if(ds==null){
+        ds = new BasicDataSource();
+        ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        ds.setUsername("root");
+        ds.setPassword("");
+        ds.setUrl("jdbc:mysql://localhost:3306/test");
+        ds.setInitialSize(50);
+        ds.setMaxIdle(10);
+        ds.setMaxTotal(20);
+        ds.setMaxWaitMillis(5000);
+    }
+    return ds;
+}
+    
+    public static Connection getPool() throws SQLException{
+        return getDataSource().getConnection();
+    }
 }
