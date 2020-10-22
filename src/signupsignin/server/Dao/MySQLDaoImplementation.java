93198b6da1ad13e32bf91a53bd44e384ee6acddc
Imanol <56909426+moxarlos@users.noreply.github.com>
2020-10-22 20:56:22 +0200

    Metodo para probar ConnectionPool
    Se ha escrito un método de prueba para comprobar que el pool de conexiones funciona correctamente.

--- src/signupsignin/server/Dao/MySQLDaoImplementation.java
+++ src/signupsignin/server/Dao/MySQLDaoImplementation.java
@@ -7,11 +7,9 @@ package signupsignin.server.Dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import org.apache.commons.dbcp2.BasicDataSource;
+
 
 /**
  *
@@ -19,11 +17,47 @@ import org.apache.commons.dbcp2.BasicDataSource;
  */
 public class MySQLDaoImplementation {
 
-    protected ConnectionPool connectionPool = new ConnectionPool();
-    protected Statement stmt;
+    private PreparedStatement ps;
+    private ResultSet rs;
+    private Connection con;
+    
+    public void doQuery() {
 
-    public MySQLDaoImplementation() {
+        try {   
 
+            // Obtengo una conexión desde el pool de conexiones.
+            con = ConnectionPool.getConnection();
+ 
+            //Establezco el preparedstatement y ejecuto la query. Preguntar ExecuteUpdate.
+            ps = con.prepareStatement("SELECT * FROM PAIS");
+            rs = ps.executeQuery();
+            
+            //Compruebo que está todo correcto en la terminal, y que la query funciona.
+            while (rs.next()) {
+                System.out.println("Username: " + rs.getString("IDIOMA"));
+            }
+            
+            System.out.println("\n=====Releasing Connection Object To Pool=====\n");            
+        } catch(SQLException sqlException) {
+            sqlException.printStackTrace();
+        } finally {
+            try {
+                // Cerrar ResulSet
+                if(rs != null) {
+                    rs.close();
+                }
+                // Cerrar PreparedStatement
+                if(ps != null) {
+                    ps.close();
+                }
+                // Cerrar conexión recibida
+                if(con != null) {
+                    con.close();
+                }
+            } catch(SQLException sqlException) {
+                sqlException.printStackTrace();
+            }
+        }
     }
 
 }
