e7a969ab97c1d5a6e7134668adf3e36c5fd161b9
Iker <theofficialdork@hotmail.com>
2020-10-26 17:01:42 +0100

    Funcionamiento del socket hecha, falta pasarlo a la estructura acorde con el proyecto y generando los hilos

--- src/signupsignin/server/Application.java
+++ src/signupsignin/server/Application.java
@@ -5,6 +5,13 @@
  */
 package signupsignin.server;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import message.Message;
 import signupsignin.server.Dao.MySQLDaoImplementation;
 
 /**
@@ -15,9 +22,17 @@ public class Application {
 
     /**
      * @param args the command line arguments
+     * @throws IOException
      */
-    public static void main(String[] args) {
-        MySQLDaoImplementation prueba = new MySQLDaoImplementation();
-        prueba.doQuery();
+    private static ServerSocket ss = null;
+
+    public static void main(String[] args) throws ClassNotFoundException, IOException {
+        ss = new ServerSocket(6666);
+        Socket socket = ss.accept();
+        ObjectInputStream is = new ObjectInputStream(socket.getInputStream());
+        Message m = (Message) is.readObject();
+        System.out.println(m.getUser().getLogin());
+        System.out.println(m.getUser().getPassword());
+        socket.close();
     }
 }
