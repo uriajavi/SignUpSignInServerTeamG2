a51d5028fd489c0e467da6f55829f73fe10a4704
Imanol <56909426+moxarlos@users.noreply.github.com>
2020-10-27 22:18:41 +0100

    Desarrollo previo a finalizar la clase
    Falta como introducir Date en la base de datos

--- src/signupsignin/server/dao/MySQLDaoImplementation.java
+++ src/signupsignin/server/dao/MySQLDaoImplementation.java
@@ -5,11 +5,20 @@
  */
 package signupsignin.server.dao;
 
+import exceptions.ErrorClosingDatabaseResources;
+import exceptions.ErrorConnectingServerException;
+import exceptions.PasswordMissmatchException;
+import exceptions.UserNotFoundException;
 import interfaces.Signable;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.sql.Date;
+import java.util.Calendar;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import user.User;
 
 /**
@@ -21,37 +30,38 @@ public class MySQLDaoImplementation implements Signable {
     private PreparedStatement ps;
     private ResultSet rs;
     private Connection con;
-    private final String searchUser = "SELECT * FROM USER WHERE LOGIN=? AND PASSWORD=?";
+    private final String checkUser = "SELECT * FROM USER WHERE LOGIN=?";
+    private final String checkPassword = "SELECT * FROM USER WHERE LOGIN=? AND PASSWORD=?";
+    private final String insertAccess = "UPDATE USER SET LASTACCESS =? WHERE LOGIN=?";
 
     @Override
-    public User signIn(User user) {
-
-        /*Metodos a implementar:
-        - Usuario no existe (select no lanzará incorrecta)
-        - Password incorrecta (select sera incongruente)
-        - Error de conexión a BDA (probar a acceder sin conexión, a ver que excepcion salta)
-        - Hay que insertar la fecha actual en el lastacces, despues de obtener el último access. Un update 
-          y controlar la excepcion de inserción.
-        - 
-        */
+    public User signIn(User user) throws ErrorConnectingServerException, UserNotFoundException, SQLException, PasswordMissmatchException, ErrorClosingDatabaseResources {
         try {
             // Obtengo una conexión desde el pool de conexiones.
             con = ConnectionPool.getConnection();
 
-            
-            //Establezco el preparedstatement y ejecuto la query.
-            ps = con.prepareStatement(searchUser);
+            //Hago comprobación de que exista el usuario.
+            checkUser(user);
+
+            //Establezco el preparedstatement y ejecuto la query. 
+            ps = con.prepareStatement(checkPassword);
             ps.setString(1, user.getLogin());
             ps.setString(2, user.getPassword());
             ResultSet rs = ps.executeQuery();
-
-            //Guardo en el objeto User el nombre y el ultimo acceso para mostrar en dashboard
-            while (rs.next()) {
-                user.setFullName(rs.getString("FULLNAME"));
-                user.setLastAccess(rs.getDate("LASTACCESS"));
+            
+            //Controlo el error de la contraseña.
+            if (!rs.next()) {
+                throw new PasswordMissmatchException();
             }
-        } catch (SQLException sqlException) {
-            sqlException.printStackTrace();
+            //Obtengo datos que voy a devolver al clente.
+            user.setFullName(rs.getString("FULLNAME"));
+            user.setLastAccess(rs.getDate("LASTACCESS"));
+            
+            insertAccesTime(user);
+           
+            //Control de error de conexión/query incorrecta.
+        } catch (SQLException ex1) {
+            throw new ErrorConnectingServerException();
         } finally {
             try {
                 // Cerrar ResulSet
@@ -66,8 +76,9 @@ public class MySQLDaoImplementation implements Signable {
                 if (con != null) {
                     con.close();
                 }
-            } catch (SQLException sqlException) {
-                sqlException.printStackTrace();
+                //
+            } catch (SQLException ex2) {
+                throw new ErrorClosingDatabaseResources();
             }
         }
 
@@ -80,4 +91,43 @@ public class MySQLDaoImplementation implements Signable {
         System.out.println("Usuario registrado");
         return null;
     }
+
+    private void checkUser(User user) throws ErrorConnectingServerException, UserNotFoundException {
+
+        try {
+            PreparedStatement ps = con.prepareStatement(checkUser);
+            ps.setString(1, user.getLogin());
+            ResultSet rs = ps.executeQuery();
+            if (!rs.next()) {
+                throw new UserNotFoundException();
+            }
+            rs.close();
+            ps.close();
+        } catch (SQLException ex3) {
+            throw new ErrorConnectingServerException();
+        }
+    }
+
+    private void insertAccesTime(User user) throws ErrorConnectingServerException {
+        //Falta conseguir introducir Date.
+        try {
+            java.sql.Date now = new java.sql.Date(System.currentTimeMillis());
+            PreparedStatement ps = con.prepareStatement(insertAccess);
+            ps.setDate(1, now);
+            ps.setString(2, user.getLogin());
+            ResultSet rs = ps.executeQuery();
+            rs.close();
+            ps.close();
+        } catch (SQLException ex3) {
+            throw new ErrorConnectingServerException();
+        }
+    }
+
 }
+
+
+
+/*host=jdbc:mysql://localhost:3306/users?useSSL=false
+user=root
+password=
+driver=com.mysql.cj.jdbc.Driver*/
