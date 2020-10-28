0bc2f2ae11c16772964efa95604bcfe44674ba0c
Mikel <mikelgmh@gmail.com>
2020-10-28 13:53:03 +0100

    Clase Application cambiada radicalmente, ahora se llama al método run desde ahí a través de una clase anónima. El worker tiene un constructor que tiene como parámetro un socket.

--- src/signupsignin/server/dao/MySQLDaoImplementation.java
+++ src/signupsignin/server/dao/MySQLDaoImplementation.java
@@ -5,11 +5,17 @@
  */
 package signupsignin.server.dao;
 
+import exceptions.ErrorConnectingServerException;
+import exceptions.ErrorConnectingDatabaseException;
+import exceptions.QueryException;
+import exceptions.UserAlreadyExistException;
 import interfaces.Signable;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import user.User;
 
 /**
@@ -22,53 +28,56 @@ public class MySQLDaoImplementation implements Signable {
     private ResultSet rs;
     private Connection con;
     private final String searchUser = "SELECT * FROM USER WHERE LOGIN=? AND PASSWORD=?";
+    private final String insertUser = "INSERT INTO user(login,email,fullname,password,status,privilege) VALUES(?,?,?,?,?,?)";
 
     @Override
     public User signIn(User user) {
 
-        try {
-            // Obtengo una conexión desde el pool de conexiones.
-            con = ConnectionPool.getConnection();
-
-            //Establezco el preparedstatement y ejecuto la query.
-            ps = con.prepareStatement(searchUser);
-            ps.setString(1, user.getLogin());
-            ps.setString(2, user.getPassword());
-            ResultSet rs = ps.executeQuery();
-
-            //Guardo en el objeto User el nombre y el ultimo acceso para mostrar en dashboard
-            while (rs.next()) {
-                user.setFullName(rs.getString("FULLNAME"));
-                user.setLastAccess(rs.getDate("LASTACCESS"));
-            }
-        } catch (SQLException sqlException) {
-            sqlException.printStackTrace();
-        } finally {
-            try {
-                // Cerrar ResulSet
-                if (rs != null) {
-                    rs.close();
-                }
-                // Cerrar PreparedStatement
-                if (ps != null) {
-                    ps.close();
-                }
-                // Cerrar conexión recibida
-                if (con != null) {
-                    con.close();
-                }
-            } catch (SQLException sqlException) {
-                sqlException.printStackTrace();
-            }
-        }
-
-        //Devuelvo user
         return user;
     }
 
     @Override
-    public User signUp(User user) {
-        System.out.println("Usuario registrado");
-        return null;
+    public User signUp(User user) throws UserAlreadyExistException, QueryException,ErrorConnectingDatabaseException {
+        try {
+            this.con = ConnectionPool.getConnection();
+            this.checkifUserExists(user);
+
+            this.ps = con.prepareStatement(this.insertUser);
+            this.ps.setString(1, user.getLogin());
+            this.ps.setString(2, user.getEmail());
+            this.ps.setString(3, user.getFullName());
+            this.ps.setString(4, user.getPassword());
+            this.ps.setString(5, user.getStatus().toString());
+            this.ps.setString(6, user.getPrivilege().toString());
+            this.ps.execute();
+            this.closeConnection();
+
+        } catch (SQLException ex) {
+            throw new QueryException();
+        }
+        return user;
+    }
+
+    private void checkifUserExists(User user) throws SQLException, UserAlreadyExistException {
+
+        this.ps = con.prepareStatement(this.insertUser);
+        this.ps.setString(1, user.getLogin());
+        this.ps.setString(2, user.getEmail());
+        this.ps.setString(3, user.getFullName());
+        this.ps.setString(4, user.getPassword());
+        this.ps.setString(5, user.getStatus().toString());
+        this.ps.setString(6, user.getPrivilege().toString());
+        this.rs = this.ps.executeQuery();
+
+        while (rs.next()) {
+            throw new UserAlreadyExistException(user);
+        }
+
+    }
+
+    private void closeConnection() throws SQLException {
+        this.rs.close();
+        this.ps.close();
+        this.con.close();
     }
 }
