36d1303664b838baa3348846e7844157a614dd4e
Mikel <mikelgmh@gmail.com>
2020-10-30 12:57:50 +0100

    Application sin el socket close

--- src/signupsignin/server/Application.java
+++ src/signupsignin/server/Application.java
@@ -6,13 +6,9 @@
 package signupsignin.server;
 
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-
-import message.Message;
-import message.TypeMessage;
+import exceptions.UserAlreadyExistException;
 
 /**
  *
@@ -21,19 +17,24 @@ import message.TypeMessage;
 public class Application {
 
     //static ServerSocket variable
-    private static ServerSocket server;
+    private static ServerSocket serverSocket;
     //socket server port on which it will listen
-    private static int port = 3333;
+    private static final int port = 3333;
 
-    public static void main(String args[]) throws IOException, ClassNotFoundException {
-        //create the socket server object
-        server = new ServerSocket(port);
-        //keep listens indefinitely until receives 'exit' call or program terminates
-        while (true) {
-            //creating socket and waiting for client connection
-            Socket socket = server.accept();
-            new Worker(socket).start();
-            socket.close();
+    public static void main(String args[]) throws IOException {
+        try {
+            //create the socket server object
+            serverSocket = new ServerSocket(port);
+            //keep listens indefinitely until receives 'exit' call or program terminates
+            while (true) {
+                //creating socket and waiting for client connection
+                Socket socket = serverSocket.accept();
+                new Worker(socket).start();
+            }
+        } catch (Exception e) {
+
+        } finally {
+
         }
 
     }
