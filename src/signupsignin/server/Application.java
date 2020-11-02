f2f4d93183cdfde541ba167d401305c6fae4aa93
Mikel <mikelgmh@gmail.com>
2020-11-03 00:25:16 +0100

    Archivo de propiedades para sereversockets

--- src/signupsignin/server/Application.java
+++ src/signupsignin/server/Application.java
@@ -9,22 +9,24 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import exceptions.UserAlreadyExistException;
+import java.util.ResourceBundle;
 
 /**
  * Application class.
+ *
  * @author Mikel
  */
 public class Application {
 
     //static ServerSocket variable
     private static ServerSocket serverSocket;
+    private static final ResourceBundle rb = ResourceBundle.getBundle("config.config");
     //socket server port on which it will listen
-    private static final int port = 3333;
-
+    
     public static void main(String args[]) throws IOException {
         try {
             //create the socket server object
-            serverSocket = new ServerSocket(port);
+            serverSocket = new ServerSocket(Integer.parseInt(rb.getString("SERVER_SOCKET_PORT")));
             //keep listens indefinitely until receives 'exit' call or program terminates
             while (true) {
                 //creating socket and waiting for client connection
@@ -32,10 +34,10 @@ public class Application {
                 new Worker(socket).start();
             }
         } catch (Exception e) {
-
+            
         } finally {
-
+            
         }
-
+        
     }
 }
